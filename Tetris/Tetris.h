9dfe0327eab8dbcfdf8ee99ed2fc701866f52298
Đinh Ngọc Đức <22028166@vnu.edu.vn>
2025-07-27 18:55:12 +0700

    add

---
+++ Tetris/Tetris.h
@@ -0,0 +1,135 @@
+#pragma once
+#ifndef TETRIS_H_
+#define TETRIS_H_
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+#include <map>
+#include "textbox.h"
+#include "sound.h"
+
+class Tetris {
+public:
+	Tetris() {
+		window = NULL;
+		render = NULL;
+		back1 = NULL;
+		blocks = NULL;
+		back2 = NULL;
+		imgplay1 = NULL;
+		imgplay2 = NULL;
+		inhelp1 = NULL;
+		inhelp2 = NULL;
+		srcR.x = 0;
+		srcR.y = 0;
+		srcR.w = BlockW;
+		srcR.h = BlockH;
+		destR.x = 0;
+		destR.y = 0;
+		destR.w = BlockW;
+		destR.h = BlockH;
+		inback1 = false;
+		running = false;
+		for (int i = 0; i < Lines; i++) {
+			for (int j = 0; j < Cols; j++) matrix[i][j] = 0;
+		}
+		color = 1;
+		move = 0;
+		score = 0;
+		ispaused = false;
+		wait = true;
+		rotate = false;
+		isPlayed = false;
+		delay = 300;
+		startTime = 0, currentTime = 0;
+
+		choosingTheme = false;
+		currentTheme = CLASSIC;
+		highScores = {
+			{"Classic", 0},
+			{"Neon", 0},
+			{"Retro", 0}
+		};
+	}
+
+	~Tetris() {}
+
+	void setCurrentTime(Uint32 t);
+
+	bool Tetrisrunning();
+	bool Tetrisback1();
+	void Setback1(bool x);
+	bool Tetrisvalid();
+	bool init(const char* name);
+	void Loadingback1();
+
+	void Randomblocks();
+
+	void handleEvennts();
+	void SetPosRect(SDL_Rect& rect, int x = 0, int y = 0, int w = BlockW, int h = BlockH);
+	void Moveblocks(SDL_Rect& rect, int x, int y); // Di chuyen cac khoi block
+
+	void Gameplay();
+
+	void GameOver();
+	void Endgame();
+	void UpdateRender();
+
+	void Clean();
+	bool TetrisWait();
+
+	void SaveHighScore();
+	void LoadHighScore();
+
+	void ChooseTheme();
+	void ApplyTheme();
+
+	bool isChoosingTheme() const {
+		return choosingTheme;
+	}
+
+	string GetExeDir();
+
+private:
+	enum {
+		ScreenW = 600,
+		ScreenH = 800
+	};
+	enum {
+		BlockW = 36,
+		BlockH = 36
+	};
+	enum {
+		Lines = 20,
+		Cols = 10
+	};
+	enum Theme { CLASSIC, NEON, RETRO };
+	bool choosingTheme;
+	Theme currentTheme;
+	map<string, int> highScores;
+
+
+	SDL_Window* window;
+	SDL_Renderer* render;
+	SDL_Texture* back1, * blocks, * back2, * imgplay1, * imgplay2, * inhelp1, * inhelp2;
+	SDL_Rect srcR, destR;
+
+	bool inback1, running;
+	int matrix[Lines][Cols];
+	static const int shapes[7][4];
+	struct Point {
+		int x, y;
+	}
+	items[4], backup[4];
+	textbox txb1, txb2, txb_gameover, inhelp, txb_high1, txb_high2, classic, neon, retro;
+
+	sound Sound;
+	int xpos, ypos;
+	int color, move, score;
+	bool ispaused, wait, rotate, isPlayed;
+	unsigned int delay;
+	Uint32 startTime, currentTime;
+};
+
+#endif // TETRIS_H_
---
+++ Tetris/main.cpp
@@ -0,0 +1,39 @@
+#include "Tetris.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+#undef main
+int main(int argc, char* argv[]) {
+    srand(time(0));
+    Tetris* game = new Tetris();
+    const char* name = "Tetris";
+    game->init(name);
+    while (game->TetrisWait()) {
+        while (game->Tetrisrunning()) {
+            game->setCurrentTime(SDL_GetTicks());
+            game->handleEvennts();
+            if (game->Tetrisback1()) {
+                game->Loadingback1();
+            }
+            else if (game->isChoosingTheme()) {
+                game->ChooseTheme();
+            }
+            else {
+                game->Gameplay();
+                game->UpdateRender();
+            }
+            game->GameOver();
+        }
+        game->Endgame();
+        SDL_Event e;
+        if (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                break;
+            }
+        }
+    }
+    game->Clean();
+    return 0;
+}
